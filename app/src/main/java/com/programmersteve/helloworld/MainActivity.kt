24ec574a35d7954186256590079e8938d2996f0f
ProgrammerSteven <Stevenmb1995@gmail.com>
2023-07-15 11:47:01 -0500

    Added button and EditText to change TextView in kotlin

--- app/src/main/java/com/programmersteve/helloworld/MainActivity.kt
+++ app/src/main/java/com/programmersteve/helloworld/MainActivity.kt
@@ -2,11 +2,21 @@ package com.programmersteve.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+    }
+    public fun onBtnClick (view:View){
+        var txtHello:TextView = findViewById(R.id.txtMessage)
+        var editText:EditText = findViewById(R.id.Message)
+        txtHello.setText("Hello "+ editText.getText().toString())
     }
 }
 
